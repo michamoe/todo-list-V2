38036acdaceef648c7154d4d0863f0fe159a9c7d
michamoe <98765003+michamoe@users.noreply.github.com>
2023-08-23 15:49:03 +0200

    newEdit

--- script.js
+++ script.js
@@ -10,23 +10,37 @@ todolist.addEventListener("submit", (e) => {
 
 function addToList(){
     let listItem = document.createElement("li");
+    let buttons = document.createElement("div");
     listItem.textContent = todoText.value;
     list.appendChild(listItem);
     todoText.value = "";
     let deleteListItem = document.createElement("button");
+    deleteListItem.setAttribute("id", "deleteButton")
     deleteListItem.textContent = "Delete";
-    listItem.appendChild(deleteListItem);
+    buttons.appendChild(deleteListItem);
+    let editListItem = document.createElement("button");
+    editListItem.setAttribute("id", "editButton")
+    editListItem.textContent = "Edit";
+    buttons.appendChild(editListItem);
+    editListItem.addEventListener("click", (e) => {
+        //e.target.parentElement.innerHTML += "Hallo"; //div
+        let currentLi = e.target.parentElement.parentElement;
+        let buttonDiv = currentLi.removeChild(e.target.parentElement);
+        currentLi.innerHTML += "Hallo"; // edit-Fenster todo;
+        currentLi.appendChild(buttonDiv);
+    });
     deleteListItem.addEventListener("click", (e) => {
-        e.target.parentElement.remove();
+        e.target.parentElement.parentElement.remove();
     })
     let doneListItem = document.createElement("button");
+    doneListItem.setAttribute("id", "doneButton")
     doneListItem.textContent = "Done";
-    listItem.appendChild(doneListItem);
+    buttons.appendChild(doneListItem);
     doneListItem.addEventListener("click", (e) => {
-        list.removeChild(e.target.parentElement); 
-        doneList.appendChild(e.target.parentElement);
-        e.target.remove();
-    })
+        doneList.appendChild(list.removeChild(e.target.parentElement.parentElement));
+        e.target.remove();  //remove of done button
+    });
+    listItem.appendChild(buttons)
 }
 
 
